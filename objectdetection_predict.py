b399a84e8d201bf7326bcedf8e1630b50c550ec6
Muhammad Hassan <mohammad555.hassan@gmail.com>
2024-11-06 01:33:42 +0500

    Update objectdetection_predict.py

--- objectdetection_predict.py
+++ objectdetection_predict.py
@@ -1,8 +1,8 @@
 import ultralytics 
 from ultralytics import YOLO
 
-model = YOLO("bottle.pt")  #load your custom-trained model
+model = YOLO("bottle.pt")  # to load your custom-trained model
 
-source = 0 #for webcam footage
+source = 0 # for webcam footage
 
 results=model.predict(source, save=True, stream_buffer=True, show=True, conf=0.5,) #args for testing. 
